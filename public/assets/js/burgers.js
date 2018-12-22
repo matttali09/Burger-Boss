fb8673e7b0426ab18aa6ac21fc769b7bcf255ed8
matttali09 <matttaliancich09@gmail.com>
2018-12-21 23:13:51 -0500

    fixed create function and put in work on the delete field

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,61 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".devour").on("click", function(event) {
+      var id = $(this).data("id");
+      var newDevour = $(this).data("newdevour");
+  
+      var newDevourState = {
+        devoured: newDevour
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevourState
+      }).then(
+        function() {
+          console.log("changed devour to", newDevour);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurg = {
+        burger_name: $("#burg").val().trim(),
+      };
+      
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurg
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".delete-form").on("submit", function(event) {
+      event.preventDefault();
+
+      // Send the DELETE request.
+      $.ajax("/api/burgers/" + $("#del").val().trim(), {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("deleted burger", burger_name);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
